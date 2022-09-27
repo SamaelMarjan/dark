f8206746ba2599ae79c3e2a47cc04fa978ebe51a
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-27 16:21:57 +0600

    home-update-sept-27-16-07

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <>
         <div className='bg-[#1f1f1f] w-[100%] h-[60px]'>
             <div className='w-[90%] h-[100%] m-auto flex items-center justify-between'>
-                <div className='bg-[#6A6D9E] w-[30px] h-[30px]'>
+                <div className='bg-[#3c6a01] w-[30px] h-[30px]'>
                     
                 </div>
                 <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
   return (
     <>
     <div id='home'>
-        <div className='bg-[#2d2d2d] w-[100%] h-[700px]'>
+        <div className='bg-[#2d2d2d] w-[100%] h-[800px] md:h-[670px]'>
             <div className='w-[90%] h-[100%] m-auto flex flex-col md:flex-row items-center justify-center gap-4'>
-                <div className='gap-4 w-[100%] md:w-[50%] h-[300px] md:h-[400px] flex flex-col items-center justify-center md:items-start'>
-                    <p className='w-[100%] md:w[90%] h-[50px] md:h-[35%] font-bold text-xl md:text-5xl'>Desenvolvimento de Sistemas Web e Mobile</p>
+                <div className='gap-4 w-[100%] md:w-[50%] h-[300px] md:h-[400px] flex flex-col items-center justify-center md:items-start text-[#D3D3D3]'>
+                    <p className='w-[100%] md:w[90%] h-[50px] md:h-[35%] font-bold text-xl md:text-5xl text-[#D3D3D3]'>Desenvolvimento de Sistemas Web e Mobile</p>
                     <p className='font-normal text-lg md:h-[20%]'>Atuamos no desenvolvimento de sistemas web, mobile e integração de banco de dados, sob medida e sob demanda.</p>
-                    <a href='a' className=' w-[200px] h-[50px] md:h-[50px] bg-[#555555] flex justify-center items-center'>SABER MAIS AGORA</a>
+                    <a href='a' className='rounded-lg w-[200px] h-[50px] md:h-[50px] bg-[#555555] flex justify-center items-center hover:bg-[#3c6a01]'>SABER MAIS AGORA</a>
                 </div>
-                <div className='bg-[#6A6D9E] w-[100%] md:w-[50%] h-[400px] md:h-[600px] flex items-center justify-center'> 
+                <div className='bg-[#3c6a01] rounded-lg w-[100%] md:w-[50%] h-[400px] md:h-[600px] flex items-center justify-center'> 
                     Image/Text
                 </div>
             </div>
