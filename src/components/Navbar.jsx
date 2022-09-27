4e89bcebe1e4a4a3d7df7939d4f7a3a729029447
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-27 13:28:24 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='bg-[#E7E7E7] w-[100%] h-[60px]'>
+            <div>
+                <div className='bg-[#6A6D9E] w-[30px] h-[30px]'>
+                    
+                </div>
+                <div>
+                    <div>
+                        <a href='a'>Home</a>
+                        <a href='b'>Services</a>
+                        <a href='c'>Contacts</a>
+                        <a href='d'>Events</a>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Navbar;
